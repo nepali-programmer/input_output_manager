5f84605a415a0e6f731ca16f61c27a76cc3253a8
Sijan Joshi <94091594+nepali-programmer@users.noreply.github.com>
2023-09-22 10:46:20 +0545

    created example

---
+++ example/lib/android_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AndroidScreen extends StatelessWidget {
+  const AndroidScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
